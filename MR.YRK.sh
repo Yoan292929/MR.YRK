b95d0a452945d0c4e895ddd046bfd454bd04d355
Yoan292929 <49474294+Yoan292929@users.noreply.github.com>
2019-06-04 13:35:48 +0700

    Create MR.YRK.sh

---
+++ MR.YRK.sh
@@ -0,0 +1,109 @@
+clear
+figlet MR.YRK | lolcat
+
+
+echo "---------------------------------------------
+# Tools : MR.YRK
+# Github  : Https://github.com/yoan292929/
+# Instagram : @yoan_rukhiyat
+----------------------------------------------" | lolcat
+
+
+###################################################
+# CTRL + C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo -e $red"[#]> (Ctrl + C ) Detected, Trying To Exit ... "
+echo -e $cyan"[#]> Thanks"
+sleep 1
+echo ""
+echo -e $white"[#]> see you gaes :)..."
+sleep 1
+exit
+}
+
+lagi=1
+while [ $lagi -lt 6 ];
+do
+echo ""
+echo  $b "1.  Nmap${enda}";
+echo  "============================" | lolcat
+echo  $r "2.  Admin-finder${endc}";
+echo  "============================" | lolcat
+echo  $g "3.  RED_HAWK${endc}";
+echo  "============================" | lolcat
+echo  $c "4   Lazymux${endc}";
+echo  "============================" | lolcat
+echo  $r"5.  Tools-X${endc}";
+echo  "============================" | lolcat
+echo  $r "6. Exit${endc}";
+echo ""
+echo  "╭─MR.YRK" |lolcat
+read -p "╰─#" pil;
+
+# Nmap
+
+case $pil in
+1) pkg install nmap
+echo -e  "${y} {1} Masukkan Web${endc}:"
+read web
+nmap $web
+echo
+
+;;
+
+# admin-finder
+
+2) git clone  https://github.com/the-c0d3r/admin-finder.git
+echo -e "${y} cara menggunakan admin finder"
+echo -e "${y} cd admin-finder"
+echo -e "${y} python admin-finder.py"
+cd /data/data/com.termux/files/home/admin-finder/
+python2 /data/data/com.termux/files/home/admin-finder/admin-finder.py
+echo
+
+;;
+
+#RED_HAWK
+
+3) git clone https://github.com/Tuhinshubhra/RED_HAWK
+echo -e "${y} Installer RED_HAWK..."
+echo -e "${y} cd RED_HAWK"
+echo -e "${y} php RED_HAWK.php"
+cd /data/data/com.termux/files/home/RED_HAWK/
+php /data/data/com.termux/files/home/RED_HAWK/ RED_HAWK.php
+
+;;
+
+#Lazymux
+
+4) git clone https://github.com/Gameye98/Lazymux
+echo -e "${y} Installer Lazymux..."
+echo -e "${y} cd Lazymux"
+echo -e "${y} python lazymux.py"
+cd /data/data/com.termux/files/home/Lazymux/
+python2 /data/data/com.termux/files/home/Lazymux/ lazymux.py
+
+;;
+
+#Tools-X
+
+5) git clone https://github.com/Rajkumrdusad/Tool-X
+echo -e "${y} Installer Tool-X..."
+echo -e "${y} cd Tool-X"
+echo -e "${y} sh install.aex"
+cd /data/data/com.termux/files/home/Tool-X
+bash /data/data/com.termux/files/home/Tool-X/sh install.aex
+
+;;
+
+
+6) echo "created by : MR.YRK" | lolcat
+exit                                                       ;;
+
+*) echo "sorry, pilihan yang anda cari tidak ada"
+esac
+done
+done
